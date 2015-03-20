f2b2d07ab2346374c15195cbcd99fb370d1e56a2
makalaaneesh <makalaaneesh@yahoo.com>
2015-03-21 00:26:31 +0530

    dbms

--- dbms_attr_clousure.py
+++ dbms_attr_clousure.py
@@ -10,7 +10,19 @@
 class graph:
 	graph_dict={}
 	visited=[]
-	def __init__(self):
+
+	def prepare(self):
+		self.graph_dict={
+			'ssn pnumber' : ['ssn', 'pnumber', 'hours'],
+			'ename' : [],
+			'plocation' : [],
+			'hours' : [],
+			'ssn' : ['ename'],
+			'pname' : [],
+			'pnumber' : ['plocation', 'pname'],
+
+		}
+	def create(self):
 		n=int(raw_input("Enter how many nodes\n"))
 		for i in range(0,n):
 			l=raw_input("\nenter left side of fd with spaces\n")
@@ -20,20 +32,28 @@ class graph:
 
 	def print_graph(self):
 		for key in self.graph_dict.keys():
-			print key, ' -> ', self.graph_dict[key]
+			print key, '->', self.graph_dict[key]
 
 	def dfs(self,s):
+		print s,","
 		self.visited.append(s)
 		for value in self.graph_dict[s]:
 			if value not in self.visited:
-				print value,","
 				self.dfs(value)
 
 
 if __name__ =='__main__':
 	fd= graph()
+	# fd.prepare()
+	fd.create()
 	fd.print_graph()
-	fd.dfs("ssn pnumber")
+	print "\n"
+	for key in fd.graph_dict.keys():
+		print "closure of",key,"is--------------"
+		fd.dfs(key)
+		fd.visited=[]
+		print "---------------------------------"
+
 	
 
 
