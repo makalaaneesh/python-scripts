1dee4ffbc9dc8ec8d4af2a83469bc396d2f08159
makalaaneesh <makalaaneesh@yahoo.com>
2015-10-30 12:23:53 +0530

    added mount command and unlink

--- friends.sh
+++ friends.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+sudo mkdir /media/makala/WINDOWS
+sudo mount -t ntfs-3g -o ro /dev/sda4 /media/makala/WINDOWS
+
+
+
 #creating the link
-sudo ln -s /media/makala/WINDOWS/Users/user/Downloads/Friends\ Complete\ Seasons\ 1-10\ Uncut\ DVDRip\ -\ 480p/ 
+cd 
+sudo ln -s /media/makala/WINDOWS/Users/user/Downloads/Friends\ Complete\ Seasons\ 1-10\ Uncut\ DVDRip\ -\ 480p/ Desktop/Friends\ Complete\ Seasons\ 1-10\ Uncut\ DVDRip\ -\ 480p
 
 #cd into a random directory
-cd Friends\ Complete\ Seasons\ 1-10\ Uncut\ DVDRip\ -\ 480p
+cd Desktop/Friends\ Complete\ Seasons\ 1-10\ Uncut\ DVDRip\ -\ 480p
 dirs=(*/) #storing all the directories in the pwd into dirs
 N=${#dirs[@]}           # Number of members in the array
 ((N=RANDOM%N)) #random
@@ -19,3 +25,5 @@ randomfile=${files[$N]}
 echo $randomfile
 vlc "$randomfile"
 
+cd ../../
+rm Friends\ Complete\ Seasons\ 1-10\ Uncut\ DVDRip\ -\ 480p 
