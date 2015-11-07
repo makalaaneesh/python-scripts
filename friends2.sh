cc285be02f35a363964595342fb9a09b2ea84f86
makalaaneesh <makalaaneesh@yahoo.com>
2015-11-07 23:12:58 +0530

    friends dialogue script

---
+++ friends2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 
+# Script that will take an input of a phrase from friends and open up the 
+# episode in which the phrase was first said!
+# 
+
+cd
+cd Friends\ Complete\ Seasons\ 1-10\ Uncut\ DVDRip\ -\ 480p
+
+echo -n "Enter keyword for Friends episode "
+read text
+echo "$text"
+var=$(find . -name *.srt | xargs -d "\n" egrep -i '.*'"$text"'.*')
+echo "$var" | cut -d ":" -f2 | head -1
+file=$(echo "$var" | cut -d ":" -f1 | head -1 )
+len="${#file}"
+if [ "$len" == 0 ]
+	then
+	 	echo "no file found"
+	else
+		vlc  "${file%.*}.mkv"
+fi
