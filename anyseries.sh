cff7adba6fef7cde2503cb33893819c7af119eb7
acemaster <vivekhtc25@gmail.com>
2015-11-10 03:57:02 +0530

    Fixes

--- anyseries.sh
+++ anyseries.sh
@@ -12,7 +12,9 @@ echo -n "Enter keyword for Any episode of the show "
 echo "Current directory: $(pwd)"
 read text
 echo "$text"
-var=$(find . -name *.srt | xargs -d "\n" egrep -i '.*'"$text"'.*')
+var=$(grep --include=\*.{srt,} -rnw "$(pwd)" -e "$text")
+echo "SRT file is: "
+echo "$var"
 echo "$var" | cut -d ":" -f2 | head -1
 file=$(echo "$var" | cut -d ":" -f1 | head -1 )
 len="${#file}"
@@ -20,5 +22,8 @@ if [ "$len" == 0 ]
 	then
 	 	echo "no file found"
 	else
-		vlc  "${file%.*}.mkv"
+		# vlc  "${file%.*}.mkv"
+		echo "${file%.*}.mkv"
 fi
+
+#vlc filename --start-time=time in secs
