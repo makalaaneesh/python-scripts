7567ea530372c46ad5f02621cb1416a291207eb4
makalaaneesh <makalaaneesh@yahoo.com>
2015-12-13 23:07:38 +0530

    lyrics working but needs refining

--- lyrics.py
+++ lyrics.py
@@ -1,6 +1,12 @@
+#
+# Needs work on 
+# 1. parsing the main aritst when artist is of the format -"xyz ft. abc"
+# 2. support for bad tags
+#
 import requests
 from xml.dom import minidom
 from bs4 import BeautifulSoup
+import sys
 
 def getXML(track, artist):
 	url = "http://lyrics.wikia.com/api.php?action=lyrics&artist="+artist+"&song="+track+"&fmt=xml"
@@ -21,17 +27,23 @@ def getLyrics(url):
 	soup = BeautifulSoup(r.content)
 	divs = soup.find_all("div", { "class" : "lyricbox" })
 	if(len(divs) == 0):
-		print "NO LYRICS FOUND"
-		return
+		return "NO LYRICS FOUND"
 	l = divs[0].contents
 	lyrics_unrefined =  l[1:len(l)-5]
 	lyrics_refined = [lyric.encode('utf-8') for lyric in lyrics_unrefined if '<br' not in lyric.encode('utf-8')]
 	return  "\n".join(lyrics_refined)
 
 if __name__ == "__main__":
-	track = "can't get enough" # track name 
-	artist = "j cole" # artist. ( only the main aritst )
-	xml = getXML(track,artist)
-	url = getURL(xml)
-	lyrics = getLyrics(url)
-	print lyrics
+	# track = "adventure of a lifetime" # track name 
+	# artist = "coldplay" # artist. ( only the main aritst )
+	if(len(sys.argv) > 3):
+		args = sys.argv
+		track = "".join(args[1:args.index("|")])
+		artist = "".join(args[args.index("|")+1:])
+		# print "python", track, artist
+		xml = getXML(track,artist)
+		url = getURL(xml)
+		lyrics = getLyrics(url)
+		print lyrics
+	else:
+		print "IMPROPER TAGS"
---
+++ lyrics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# A script that will fetch lyrics with file as argument 
+# requires mp3info Run sudo-apt get install mp3info if not installed
+
+artist=$(mp3info -p %a "$1")
+title=$(mp3info -p %t "$1")
+echo "aritst is $artist and title is $title"
+python lyrics.py "$title" "|" "$artist"
